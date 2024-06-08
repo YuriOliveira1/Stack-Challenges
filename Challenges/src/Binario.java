2d77b7d5b685e400629976c307927a94ac303656
Yuri de Oliveira <161542848+YuriOliveira1@users.noreply.github.com>
2024-06-07 22:32:00 -0300

    Desafio de Binario

---
+++ Challenges/src/Binario.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Binario {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Qual número decimal voce quer converter? ");
+        int numeroDecimal = scan.nextInt();
+        System.out.println("Convetido em Binario " + converteEmBinario(numeroDecimal));
+    }
+
+    public static String converteEmBinario(int numeroDecimal) {
+        int divisor = 2;
+        int resto;
+        String numeroBinario = "";
+
+        Stack<Integer> stackBinarios = new Stack<>();
+
+        while (numeroDecimal > 0) {
+            resto = numeroDecimal % divisor;
+            stackBinarios.push(resto);
+            numeroDecimal /= 2;
+        }
+
+        while (!stackBinarios.isEmpty()) {
+            numeroBinario += stackBinarios.pop();
+        }
+        
+        return numeroBinario;
+    }
+}
